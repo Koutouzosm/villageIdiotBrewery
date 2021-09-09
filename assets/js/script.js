093f3ffa7087b45e2dbf3a311475ed4abaef2456
Mano <Koutouzosm@gmail.com>
2021-09-09 14:22:33 -0400

    looped through API list and retrieved beer data

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,43 +12,48 @@ function renderBeerDisplay(data) {
             return response.json();
         })
         .then(function (data) {
+
             console.log(data);
-            // Store response data from our fetch request in variables
-            var beverageLogo = data[0].MenuItemProductDetail.Beverage.LogoImageUrl;
-            var beverage = data[0].MenuItemProductDetail.Beverage.BeverageName;
-            var beverageStyle = data[0].MenuItemProductDetail.Beverage.BeerStyle.StyleName;
-            console.log(beverageLogo)
-            console.log(beverage)
-            console.log(beverageStyle)
+
+            for (let i = 0; i < data.length; i++) {
+                // Store response data from our fetch request in variables
+                var beverageLogo = data[i].MenuItemProductDetail.Beverage.LogoImageUrl;
+                var beverage = data[i].MenuItemProductDetail.Beverage.BeverageName;
+                var beverageStyle = data[i].MenuItemProductDetail.Beverage.BeerStyle.StyleName;
+                console.log(beverageLogo)
+                console.log(beverage)
+                console.log(beverageStyle)
 
 
-            var col = document.createElement('div');
-            var card = document.createElement('div');
-            var cardBody = document.createElement('div');
-            var cardTitle = document.createElement('h5');
-            var beerIcon = document.createElement('img');
-            var beerStyleEl = document.createElement('p');
-          
-        
-            col.append(card);
-            card.append(cardBody);
-            cardBody.append(cardTitle, beerIcon, beerStyleEl);
-          
-            col.setAttribute('class', 'col-md');
-            col.classList.add('beer-card');
-            card.setAttribute('class', 'card bg-primary h-100 text-white');
-            cardBody.setAttribute('class', 'card-body p-2');
-            cardTitle.setAttribute('class', 'card-title');
-            beerStyleEl.setAttribute('class', 'card-text');
-          
-            // Add content to elements
-            cardTitle.textContent = `${beverage}`;
-            beerIcon.setAttribute('src', beverageLogo);
-            beerIcon.setAttribute('alt', beverageLogo);
-            beerStyleEl.textContent = `${beverageStyle}`;
-       
-          
-            beerListContainer.append(col);
+                var col = document.createElement('div');
+                var card = document.createElement('div');
+                var cardBody = document.createElement('div');
+                var cardTitle = document.createElement('h5');
+                var beerIcon = document.createElement('img');
+                var beerStyleEl = document.createElement('p');
+
+
+                col.append(card);
+                card.append(cardBody);
+                cardBody.append(cardTitle, beerIcon, beerStyleEl);
+
+                col.setAttribute('class', 'col-md');
+                col.classList.add('beer-card');
+                card.setAttribute('class', 'card bg-primary h-100 text-white');
+                cardBody.setAttribute('class', 'card-body p-2');
+                cardTitle.setAttribute('class', 'card-title');
+                beerStyleEl.setAttribute('class', 'card-text');
+
+                // Add content to elements
+                cardTitle.textContent = `${beverage}`;
+                beerIcon.setAttribute('src', beverageLogo);
+                beerIcon.setAttribute('alt', beverageLogo);
+                beerStyleEl.textContent = `${beverageStyle}`;
+
+
+                beerListContainer.append(col);
+            }
+
         });
 
 }
